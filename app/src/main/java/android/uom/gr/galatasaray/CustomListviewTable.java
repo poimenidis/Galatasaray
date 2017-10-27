48339fdb73e754ee6de35a10bcffe71aef21a13a
Kostas Poimenidis <it1530@uom.edu.gr>
2017-10-27 18:36:41 +0300

    android2

--- app/src/main/java/android/uom/gr/galatasaray/CustomListviewTable.java
+++ app/src/main/java/android/uom/gr/galatasaray/CustomListviewTable.java
@@ -40,19 +40,16 @@ public class CustomListviewTable extends BaseAdapter {
 
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
         return data1.size();
     }
 
     @Override
     public Object getItem(int position) {
-        // TODO Auto-generated method stub
         return position;
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
         return position;
     }
 
